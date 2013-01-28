a1abc99997515ed1864ef0c70253be0773b9dc1f
Celogeek <me@celogeek.com>
2013-01-28 15:25:46 +0100

    start review of a task, fetch title, init conf

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@ cd "$(dirname "$0")"
 cpanm -nv http://cpan.metacpan.org/authors/id/C/CE/CELOGEEK/Redmine-API-0.03.tar.gz
 sudo mkdir -p /usr/local/share/Weborama-Git-Review/helpers/
 sudo cp -av helpers/* /usr/local/share/Weborama-Git-Review/helpers/
+sudo cp -av bin/* /usr/local/bin/
+sudo chmod +x /usr/local/share/Weborama-Git-Review/helpers/* /usr/local/bin/git-review /usr/local/bin/git-review-*
