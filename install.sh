fe9b1bfe48ba4cda02c1e441fe1fd6e161c7eab5
Celogeek <me@celogeek.com>
2013-01-29 12:21:52 +0100

    add netspore from cpan

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-cpanm -nv http://cpan.metacpan.org/authors/id/C/CE/CELOGEEK/Redmine-API-0.03.tar.gz
+cpanm -nv http://search.cpan.org/CPAN/authors/id/F/FR/FRANCKC/Net-HTTP-Spore-0.05.tar.gz http://cpan.metacpan.org/authors/id/C/CE/CELOGEEK/Redmine-API-0.03.tar.gz
 sudo mkdir -p /usr/local/share/Weborama-Git-Review/helpers/
 sudo cp -av helpers/* /usr/local/share/Weborama-Git-Review/helpers/
 sudo cp -av bin/* /usr/local/bin/
