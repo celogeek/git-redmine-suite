f8c3aaa556b589dfffe50965aa1549a6b6935bbc
Celogeek <me@celogeek.com>
2013-01-29 15:31:19 +0100

    add todo task

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-cpanm -nv http://search.cpan.org/CPAN/authors/id/F/FR/FRANCKC/Net-HTTP-Spore-0.05.tar.gz http://cpan.metacpan.org/authors/id/C/CE/CELOGEEK/Redmine-API-0.03.tar.gz
+[ "x$SKIP_CPANM" == "x" ] && cpanm -nv http://search.cpan.org/CPAN/authors/id/F/FR/FRANCKC/Net-HTTP-Spore-0.05.tar.gz http://cpan.metacpan.org/authors/id/C/CE/CELOGEEK/Redmine-API-0.03.tar.gz
 sudo mkdir -p /usr/local/share/Weborama-Git-Review/helpers/
 sudo cp -av helpers/* /usr/local/share/Weborama-Git-Review/helpers/
 sudo cp -av bin/* /usr/local/bin/
-sudo chmod +x /usr/local/share/Weborama-Git-Review/helpers/* /usr/local/bin/git-review /usr/local/bin/git-review-*
+sudo chmod +x /usr/local/share/Weborama-Git-Review/helpers/* /usr/local/bin/git-review /usr/local/bin/git-review-* /usr/local/bin/git-task /usr/local/bin/git-task-*
