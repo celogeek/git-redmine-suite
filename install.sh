7a65343b3a5d3bfdc3cf02bf191e528fbcdbbf8e
Celogeek <me@celogeek.com>
2013-02-01 14:24:43 +0100

    task : merge todo and inprogress

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 [ "x$SKIP_CPANM" == "x" ] && cpanm -nv http://search.cpan.org/CPAN/authors/id/F/FR/FRANCKC/Net-HTTP-Spore-0.05.tar.gz http://cpan.metacpan.org/authors/id/C/CE/CELOGEEK/Redmine-API-0.03.tar.gz
+sudo rm -rf /usr/local/share/Weborama-Git-Review/helpers/ /usr/local/bin/git-review /usr/local/bin/git-review-* /usr/local/bin/git-task /usr/local/bin/git-task-*
 sudo mkdir -p /usr/local/share/Weborama-Git-Review/helpers/
 sudo cp -av helpers/* /usr/local/share/Weborama-Git-Review/helpers/
 sudo cp -av bin/* /usr/local/bin/
