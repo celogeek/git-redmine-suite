297404236d404c94ec49ca5f306bc290358e32ff
Celogeek <me@celogeek.com>
2013-02-04 17:36:51 +0100

    add deps

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-[ "x$SKIP_CPANM" == "x" ] && sudo PERL_CPANM_OPT="" ./cpanm -nv Redmine::API
+[ "x$SKIP_CPANM" == "x" ] && sudo PERL_CPANM_OPT="" ./cpanm -nv Redmine::API MooX::Options LWP::Protocol::https
 sudo rm -rf /usr/local/share/Weborama-Git-Review/helpers/ /usr/local/bin/git-review /usr/local/bin/git-review-* /usr/local/bin/git-task /usr/local/bin/git-task-*
 sudo mkdir -p /usr/local/share/Weborama-Git-Review/helpers/
 sudo cp -av helpers/* /usr/local/share/Weborama-Git-Review/helpers/
