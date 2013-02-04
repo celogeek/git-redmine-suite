3ed9fa344574477078749327dbeaa8bf38537a3c
Celogeek <me@celogeek.com>
2013-02-04 18:29:06 +0100

    add project

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 [ "x$SKIP_CPANM" == "x" ] && sudo PERL_CPANM_OPT="" ./cpanm -nv Redmine::API MooX::Options LWP::Protocol::https
-sudo rm -rf /usr/local/share/Weborama-Git-Review/helpers/ /usr/local/bin/git-review /usr/local/bin/git-review-* /usr/local/bin/git-task /usr/local/bin/git-task-*
+sudo rm -rf /usr/local/share/Weborama-Git-Review/helpers/ /usr/local/bin/git-review /usr/local/bin/git-review-* /usr/local/bin/git-task /usr/local/bin/git-task-* /usr/local/bin/git-project /usr/local/bin/git-project-*
+
 sudo mkdir -p /usr/local/share/Weborama-Git-Review/helpers/
 sudo cp -av helpers/* /usr/local/share/Weborama-Git-Review/helpers/
 sudo cp -av bin/* /usr/local/bin/
-sudo chmod +x /usr/local/share/Weborama-Git-Review/helpers/* /usr/local/bin/git-review /usr/local/bin/git-review-* /usr/local/bin/git-task /usr/local/bin/git-task-*
+sudo chmod +x /usr/local/share/Weborama-Git-Review/helpers/* /usr/local/bin/git-review /usr/local/bin/git-review-* /usr/local/bin/git-task /usr/local/bin/git-task-* /usr/local/bin/git-project /usr/local/bin/git-project-*
