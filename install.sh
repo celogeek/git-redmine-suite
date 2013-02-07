2d6b1cf201d309718c8a3c23e3bb7f409f0561b4
Celogeek <me@celogeek.com>
2013-02-07 12:07:03 +0100

    add redmine project

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 [ "x$SKIP_CPANM" == "x" ] && sudo PERL_CPANM_OPT="" ./cpanm -nv Redmine::API MooX::Options LWP::Protocol::https
-sudo rm -rf /usr/local/share/Weborama-Git-Review/helpers/ /usr/local/bin/git-review /usr/local/bin/git-review-* /usr/local/bin/git-task /usr/local/bin/git-task-* /usr/local/bin/git-project /usr/local/bin/git-project-*
+sudo rm -rf /usr/local/share/Weborama-Git-Review/helpers/ /usr/local/bin/git-review /usr/local/bin/git-review-* /usr/local/bin/git-task /usr/local/bin/git-task-* /usr/local/bin/git-project /usr/local/bin/git-project-* /usr/local/bin/git-redmine /usr/local/bin/git-redmine-*
 
 sudo mkdir -p /usr/local/share/Weborama-Git-Review/helpers/
 sudo cp -av helpers/* /usr/local/share/Weborama-Git-Review/helpers/
 sudo cp -av bin/* /usr/local/bin/
-sudo chmod +x /usr/local/share/Weborama-Git-Review/helpers/* /usr/local/bin/git-review /usr/local/bin/git-review-* /usr/local/bin/git-task /usr/local/bin/git-task-* /usr/local/bin/git-project /usr/local/bin/git-project-*
+sudo chmod +x /usr/local/share/Weborama-Git-Review/helpers/* /usr/local/bin/git-review /usr/local/bin/git-review-* /usr/local/bin/git-task /usr/local/bin/git-task-* /usr/local/bin/git-project /usr/local/bin/git-project-* /usr/local/bin/git-redmine /usr/local/bin/git-redmine-*
