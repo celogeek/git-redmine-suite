5902c61c5c1d92874d2c0b1c18d034e60056c26d
celogeek <me@celogeek.com>
2013-02-17 21:05:08 +0100

    cleanup install

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 [ "x$SKIP_CPANM" == "x" ] && sudo PERL_CPANM_OPT="" ./cpanm -nv Redmine::API Moo MooX::Options LWP::Protocol::https Version::Next Encode DateTime
-sudo rm -rf /usr/local/share/Weborama-Git-Review /usr/local/share/Git-Redmine-Suite /usr/local/bin/git-review /usr/local/bin/git-review-* /usr/local/bin/git-task /usr/local/bin/git-task-* /usr/local/bin/git-project /usr/local/bin/git-project-* /usr/local/bin/git-redmine /usr/local/bin/git-redmine-*
+sudo rm -rf /usr/local/share/Git-Redmine-Suite usr/local/bin/git-redmine /usr/local/bin/git-redmine-*
 
 sudo mkdir -p /usr/local/share/Git-Redmine-Suite/helpers/
 sudo cp -av helpers/* /usr/local/share/Git-Redmine-Suite/helpers/
