62f916639d25b4f6e5340b084d11635861f461a4
celogeek <me@celogeek.com>
2013-02-18 23:26:49 +0100

    use pod2man, and convert pod to man during install

--- install.sh
+++ install.sh
@@ -7,3 +7,11 @@ sudo mkdir -p /usr/local/share/Git-Redmine-Suite/helpers/
 sudo cp -av helpers/* /usr/local/share/Git-Redmine-Suite/helpers/
 sudo cp -av bin/* /usr/local/bin/
 sudo chmod +x /usr/local/share/Git-Redmine-Suite/helpers/* /usr/local/bin/git-redmine /usr/local/bin/git-redmine-*
+
+POD2MAN=$(/usr/bin/perl -E 'say "/usr/bin/pod2man", sprintf("%d.%d",split(/\./, sprintf("%vd",$^V)))')
+[ -x $POD2MAN ] || POD2MAN=/usr/bin/pod2man
+for F in /usr/local/share/Git-Redmine-Suite/helpers/pod/guide/*.pod
+do
+    echo "Pod2Man : $F ..."
+    $POD2MAN "$F" "${F/.pod/.man}"
+done
