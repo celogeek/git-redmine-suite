8407552accac06b3d3cf4a53ebc5e75d9cb1be9e
celogeek <me@celogeek.com>
2013-03-06 12:59:27 +0100

    auto exec sub command after upgrade

--- install.sh
+++ install.sh
@@ -6,7 +6,9 @@ sudo rm -rf /usr/local/share/Git-Redmine-Suite usr/local/bin/git-redmine /usr/lo
 sudo mkdir -p /usr/local/share/Git-Redmine-Suite/helpers/
 sudo cp -av helpers/* /usr/local/share/Git-Redmine-Suite/helpers/
 sudo cp -av bin/* /usr/local/bin/
+sudo chown 0:0 /usr/local/share/Git-Redmine-Suite/helpers/* /usr/local/bin/git-redmine /usr/local/bin/git-redmine-*
 sudo chmod +x /usr/local/share/Git-Redmine-Suite/helpers/* /usr/local/bin/git-redmine /usr/local/bin/git-redmine-*
+sudo chmod u+s,g+s /usr/local/bin/git-redmine-self-upgrade*
 
 POD2MAN=$(/usr/bin/perl -E 'say "/usr/bin/pod2man", sprintf("%d.%d",split(/\./, sprintf("%vd",$^V)))')
 [ -x $POD2MAN ] || POD2MAN=/usr/bin/pod2man
@@ -15,3 +17,8 @@ do
     echo "Pod2Man : $F ..."
     $POD2MAN "$F" "${F/.pod/.man}"
 done
+
+if [ "x$1" == "xgit" ]
+then
+	exec $*
+fi
