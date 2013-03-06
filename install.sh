b4b7ab788c1f0e95c54f783b3a9510b10ffb0b1d
celogeek <me@celogeek.com>
2013-03-06 13:01:04 +0100

    put HOME in /tmp for cpanm

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-[ "x$SKIP_CPANM" == "x" ] && sudo PERL_CPANM_OPT="" ./cpanm -nv Redmine::API Moo MooX::Options LWP::Protocol::https Version::Next Encode DateTime Term::ReadLine
+[ "x$SKIP_CPANM" == "x" ] && sudo HOME=/tmp PERL_CPANM_OPT="" ./cpanm -nv Redmine::API Moo MooX::Options LWP::Protocol::https Version::Next Encode DateTime Term::ReadLine
 sudo rm -rf /usr/local/share/Git-Redmine-Suite usr/local/bin/git-redmine /usr/local/bin/git-redmine-*
 
 sudo mkdir -p /usr/local/share/Git-Redmine-Suite/helpers/
