f9104ca6abfea6fc49daa8e8e7ef1896aeeb1820
celogeek <me@celogeek.com>
2013-03-06 13:04:51 +0100

    fix right

--- install.sh
+++ install.sh
@@ -6,17 +6,19 @@ sudo rm -rf /usr/local/share/Git-Redmine-Suite usr/local/bin/git-redmine /usr/lo
 sudo mkdir -p /usr/local/share/Git-Redmine-Suite/helpers/
 sudo cp -av helpers/* /usr/local/share/Git-Redmine-Suite/helpers/
 sudo cp -av bin/* /usr/local/bin/
-sudo chown 0:0 /usr/local/share/Git-Redmine-Suite/helpers/* /usr/local/bin/git-redmine /usr/local/bin/git-redmine-*
-sudo chmod +x /usr/local/share/Git-Redmine-Suite/helpers/* /usr/local/bin/git-redmine /usr/local/bin/git-redmine-*
-sudo chmod u+s,g+s /usr/local/bin/git-redmine-self-upgrade*
+sudo cp -av VERSION /usr/local/share/Git-Redmine-Suite/
 
 POD2MAN=$(/usr/bin/perl -E 'say "/usr/bin/pod2man", sprintf("%d.%d",split(/\./, sprintf("%vd",$^V)))')
 [ -x $POD2MAN ] || POD2MAN=/usr/bin/pod2man
 for F in /usr/local/share/Git-Redmine-Suite/helpers/pod/guide/*.pod
 do
     echo "Pod2Man : $F ..."
-    $POD2MAN "$F" "${F/.pod/.man}"
+    sudo $POD2MAN "$F" "${F/.pod/.man}"
 done
+
+sudo chown -R 0:0 /usr/local/share/Git-Redmine-Suite/ /usr/local/bin/git-redmine /usr/local/bin/git-redmine-*
+sudo chmod -R 755 /usr/local/share/Git-Redmine-Suite/ /usr/local/bin/git-redmine /usr/local/bin/git-redmine-*
+sudo chmod u+s,g+s /usr/local/bin/git-redmine-self-upgrade*
 
 if [ "x$1" == "xgit" ]
 then
