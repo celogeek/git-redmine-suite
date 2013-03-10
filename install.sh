c6b5078315a42b5468e651af0cc2996f31c7c90a
celogeek <me@celogeek.com>
2013-03-10 16:59:40 +0100

    update deps

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-[ "x$SKIP_CPANM" == "x" ] && sudo HOME=/tmp PERL_CPANM_OPT="" ./cpanm -nv Redmine::API Moo MooX::Options LWP::Protocol::https Version::Next Encode DateTime Term::ReadLine Date::Parse LWP::Curl List::MoreUtils
+[ "x$SKIP_CPANM" == "x" ] && sudo HOME=/tmp PERL_CPANM_OPT="" ./cpanm -nv Redmine::API Moo MooX::Options LWP::Protocol::https Version::Next DateTime Term::ReadLine Date::Parse LWP::Curl List::MoreUtils autodie utf8::all Term::Size
 
 sudo rm -rf /usr/local/share/Git-Redmine-Suite usr/local/bin/git-redmine /usr/local/bin/git-redmine-*
 
