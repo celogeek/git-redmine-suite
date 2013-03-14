ee9e1030fd9a06deaf46b31d1a660caec81fa24b
celogeek <me@celogeek.com>
2013-03-14 23:05:09 +0100

    split helpers

--- share/Git-Redmine-Suite/helpers/scripts/grs.sh
+++ share/Git-Redmine-Suite/helpers/scripts/grs.sh
@@ -7,9 +7,11 @@ export PATH=$PERL_DIR/bin:$PATH
 export PERLLIB=$PERL_DIR/lib
 
 set +e
-source $SCRIPT_DIR/check_update.sh
+source "$SCRIPT_DIR"/check_update.sh
 set -e
 if [ -z "$SETUP" ]; then
-	source $SCRIPT_DIR/set_env.sh
+	source "$SCRIPT_DIR"/set_env.sh
 fi
-source $SCRIPT_DIR/helpers.sh
+for P in "$SCRIPT_DIR"/grs/*.sh; do
+	source "$P"
+done
---
+++ share/Git-Redmine-Suite/helpers/scripts/grs/task.sh
@@ -0,0 +1,32 @@
+# tasks
+function task_update
+{
+	declare -a PARAMS
+
+	PARAMS+=(--id "$task")
+	PARAMS+=(--status_id "$status")
+	PARAMS+=(--assigned_to_id "$assigned_to")
+
+	declare -a ADD_PARAMS
+	if [ "$status" = "$REDMINE_REVIEW_TODO" ]
+		then
+		ADD_PARAMS+=(--progress "100")
+	elif [ -n "$progress" ]
+		then
+		ADD_PARAMS+=(--progress "$progress")
+	fi
+
+	if [ -n "$notes" ]
+		then
+		ADD_PARAMS+=(--notes "$notes")
+	fi
+
+	if [ -n "$cf_id" ] && [ -n "$cf_val" ]
+		then
+		ADD_PARAMS+=(--cf_id "$cf_id")
+		ADD_PARAMS+=(--cf_val "$cf_val")
+	fi
+
+    redmine-update-task "${PARAMS[@]}" "${ADD_PARAMS[@]}"
+    redmine-check-task "${PARAMS[@]}"
+}
