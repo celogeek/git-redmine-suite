f664c663a2b38786836877844f93c84643fe4286
celogeek <me@celogeek.com>
2013-03-15 20:49:26 +0100

    remove set

--- share/Git-Redmine-Suite/helpers/scripts/grs.sh
+++ share/Git-Redmine-Suite/helpers/scripts/grs.sh
@@ -6,9 +6,7 @@ MAN_DIR=$ROOT_DIR/helpers/pod/guide
 export PATH=$PERL_DIR/bin:$PATH
 export PERLLIB=$PERL_DIR/lib
 
-set +e
 source "$SCRIPT_DIR"/check_update.sh
-set -e
 if [ -z "$SETUP" ]; then
 	source "$SCRIPT_DIR"/set_env.sh
 fi
