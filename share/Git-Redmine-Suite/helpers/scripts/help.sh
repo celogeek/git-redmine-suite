bd856d47f24a354aa7ff8d226ed896fd4bbdf032
celogeek <me@celogeek.com>
2013-03-17 17:20:11 +0100

    add possible_env for help

--- share/Git-Redmine-Suite/helpers/scripts/help.sh
+++ share/Git-Redmine-Suite/helpers/scripts/help.sh
@@ -1,6 +1,9 @@
 function help_command {
 	if [ -n "$HELP" ]; then
 		CMD=$(basename $0 | /usr/bin/perl -pe 's/\-/ /g')
+		if [ -n "$POSSIBLE_ENV" ]; then
+			CMD="$POSSIBLE_ENV $CMD"
+		fi
 		echo "    * $CMD $*"
 		exit 1
 	fi
