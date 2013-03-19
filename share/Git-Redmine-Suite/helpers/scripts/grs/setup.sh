abc7e35099cdf4a58e20542495e19d094478ceae
celogeek <me@celogeek.com>
2013-03-19 23:30:26 +0100

    add setup upgrade

--- share/Git-Redmine-Suite/helpers/scripts/grs/setup.sh
+++ share/Git-Redmine-Suite/helpers/scripts/grs/setup.sh
@@ -18,7 +18,7 @@ function setup_with_profile {
 		HELP=1 exec $0
 	fi
 
-	echo "Setup profile $2 with $3 ..."
+	echo "Setup profile${GLOBAL/--/ } $2 with $3 ..."
 
 	git config --remove-section redmine.user 2> /dev/null
 
@@ -38,4 +38,32 @@ function help_setup_with_profile {
 
 	echo ""
 	exit 1
+}
+
+function setup_upgrade {
+	URL=$(git config --local redmine.url)
+	AUTHKEY=$(git config --local redmine.authkey)
+	VERSION=$(git config --local redmine.version)
+	GLOBAL=""
+	if [ -z "$URL" ] || [ -z "$AUTHKEY" ]; then
+		URL=$(git config --global redmine.url)
+		AUTHKEY=$(git config --global redmine.authkey)
+		VERSION=$(git config --global redmine.version)
+		GLOBAL="--global"
+	fi
+
+	CURRENT_VERSION=$(cat "$ROOT_DIR/VERSION")
+
+	if [ "$VERSION" != "$CURRENT_VERSION" ]; then
+		git config $GLOBAL redmine.version "$CURRENT_VERSION"
+		if [ -n "$URL" ] && [ -n "$AUTHKEY" ]; then
+			PROFILE=""
+			for i in "$ROOT_DIR/helpers/setup/"*.conf; do
+				grep -q ^"redmine.url.*=.*$URL$" "$i" && PROFILE=$(basename "${i/.conf}") && break
+			done
+			if [ -n "$PROFILE" ]; then
+				setup_with_profile "$GLOBAL" "$PROFILE" "$AUTHKEY"
+			fi
+		fi
+	fi
 }
