09fe94d5d592c3d1d59c7bbbf3aae5e6eba85c03
celogeek <me@celogeek.com>
2013-03-20 23:41:40 +0100

    allow chain reassign even after an echec

--- share/Git-Redmine-Suite/helpers/scripts/grs/reassigned.sh
+++ share/Git-Redmine-Suite/helpers/scripts/grs/reassigned.sh
@@ -12,11 +12,12 @@ function reassigned_this {
 		PARAMS+=(--default_answer "$PREVIOUS_ASSIGNED_TO_ID")
 	fi
 
-	if [ -n "$REDMINE_AUTO_REASSIGN" ] && [ -n "$PREVIOUS_ASSIGNED_TO_ID" ]; then
+	if [ -n "$REDMINE_AUTO_REASSIGN" ] && [ -z "$REDMINE_NO_AUTO_REASSIGN" ] && [ -n "$PREVIOUS_ASSIGNED_TO_ID" ]; then
 		echo "${PARAMS[1]} $PREVIOUS_ASSIGNED_TO_ID"
 		echo ""
 		ASSIGNED_TO_ID="$PREVIOUS_ASSIGNED_TO_ID"
 	else
+		REDMINE_NO_AUTO_REASSIGN=""
 		ASSIGNED_TO_ID=$(ask_question "${PARAMS[@]}")
 	fi
 
@@ -25,7 +26,7 @@ function reassigned_this {
 		echo "This user is not a member of this project !"
 		if [ -n "$REDMINE_AUTO_REASSIGN" ]; then
 			echo ""
-			REDMINE_AUTO_REASSIGN=""
+			REDMINE_NO_AUTO_REASSIGN="1"
 			reassigned_this $TYPE $PROJECT
 		else
 			if [ "$TYPE" != "release" ]; then
