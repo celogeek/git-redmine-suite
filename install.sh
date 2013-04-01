aad7017a288555d4d1edf4ee81260f1966e0b569
celogeek <me@celogeek.com>
2013-04-01 12:16:04 +0200

    use date for check profile

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ fi
 
 cd "$(dirname "$0")"
 if [ -z "$SKIP_CPANM" ]; then
-	if ! sudo HOME=/tmp PERL_CPANM_OPT="" ./cpanm -nv Redmine::API Moo MooX::Options LWP::Protocol::https Version::Next DateTime Term::ReadLine Date::Parse LWP::Curl List::MoreUtils List::Util List::Util::XS autodie utf8::all Term::Size File::MkTemp; then
+	if ! sudo HOME=/tmp PERL_CPANM_OPT="" ./cpanm -nv Redmine::API Moo MooX::Options LWP::Protocol::https Version::Next DateTime Term::ReadLine Date::Parse LWP::Curl List::MoreUtils List::Util List::Util::XS autodie utf8::all Term::Size; then
 		echo "Fail to install dependencies !"
 		exit 1
 	fi
