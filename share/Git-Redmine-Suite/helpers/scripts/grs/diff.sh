d25eb1af91964f7fd928ff66f0e7a4f5fa98660f
celogeek <me@celogeek.com>
2013-04-16 01:33:15 +0200

    add diff url

--- share/Git-Redmine-Suite/helpers/scripts/grs/diff.sh
+++ share/Git-Redmine-Suite/helpers/scripts/grs/diff.sh
@@ -1,3 +1,3 @@
 function get_full_diff_url {
-	get_diff_url --diff-url="$REDMINE_GIT_DIFF" --git-remote-url="REDMINE_GIT_REPOS_URL" --ref_from="$1" --ref_to="$2"
+	get_diff_url --diff-url="$REDMINE_GIT_DIFF" --git-remote-url="$REDMINE_GIT_REPOS_URL" --ref_from="$1" --ref_to="$2"
 }
