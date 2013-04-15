c675eaf4e22931c9468bde4b8ebf65afafe3efa5
celogeek <me@celogeek.com>
2013-04-16 01:36:40 +0200

    fix empty git diff

--- share/Git-Redmine-Suite/helpers/scripts/grs/diff.sh
+++ share/Git-Redmine-Suite/helpers/scripts/grs/diff.sh
@@ -1,3 +1,3 @@
 function get_full_diff_url {
-	get_diff_url --diff-url="$REDMINE_GIT_DIFF" --git-remote-url="$REDMINE_GIT_REPOS_URL" --ref_from="$1" --ref_to="$2"
+	[ -n "$REDMINE_GIT_DIFF" ] && get_diff_url --diff-url="$REDMINE_GIT_DIFF" --git-remote-url="$REDMINE_GIT_REPOS_URL" --ref_from="$1" --ref_to="$2"
 }
