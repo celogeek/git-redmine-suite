bc627bfaebb47144499d3588b804030d621d05f9
celogeek <me@celogeek.com>
2013-06-01 13:29:22 +0200

    check if current dir is git first

--- share/Git-Redmine-Suite/helpers/scripts/status.sh
+++ share/Git-Redmine-Suite/helpers/scripts/status.sh
@@ -1,16 +1,19 @@
 function check_status_tasks {
-	LAST_CHECK=$(git config redmine.task.lastcheck)
-	if [ -z "$LAST_CHECK" ] || delta_date_check --date "$LAST_CHECK" --days "7"
-	then
-		git config redmine.task.lastcheck $(date +%Y%m%d)
-		echo "You have not check the status of your tasks since more than a week !"
-		if ask_question --question="Check Now ?"
+	GIT_DIR=$(git rev-parse --git-dir 2>/dev/null)
+	if [ -n "$GIT_DIR" ]; then
+		LAST_CHECK=$(git config redmine.task.lastcheck)
+		if [ -z "$LAST_CHECK" ] || delta_date_check --date "$LAST_CHECK" --days "7"
 		then
-			git redmine task status -f
-		else
-			echo "Please run a check from time to time to clear the remote repository and yours."
-			echo "    * git task status -f"
-			echo ""
+			git config redmine.task.lastcheck $(date +%Y%m%d)
+			echo "You have not check the status of your tasks since more than a week !"
+			if ask_question --question="Check Now ?"
+			then
+				git redmine task status -f
+			else
+				echo "Please run a check from time to time to clear the remote repository and yours."
+				echo "    * git task status -f"
+				echo ""
+			fi
 		fi
 	fi
 }
