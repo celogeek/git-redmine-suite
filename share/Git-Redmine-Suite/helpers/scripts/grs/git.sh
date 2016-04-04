f162505ca76f928d837e75cde21bc3cc081d9e0e
Celogeek <me@celogeek.com>
2016-04-04 11:18:07 +0200

    add script to check local changes

--- share/Git-Redmine-Suite/helpers/scripts/grs/git.sh
+++ share/Git-Redmine-Suite/helpers/scripts/grs/git.sh
@@ -2,3 +2,25 @@ function git_refresh_local_repos {
 	git fetch -ap
 	git fetch --tags -p
 }
+
+function git_has_local_changes {
+  git_status=$(git status --untracked-files=no --porcelain)
+  if [ -n "$git_status" ]
+  then
+    echo "Your local branch is not clean:"
+    echo "$git_status"
+    echo ""
+    echo "Please commit and push first."
+    exit 1
+  fi
+
+  git_cherry=$(git cherry -v --abbrev)
+  if [ -n "$git_cherry" ]
+  then
+    echo "There is pending commit:"
+    echo "$git_cherry"
+    echo ""
+    echo "Please push this first ..."
+    exit 1
+  fi
+}
