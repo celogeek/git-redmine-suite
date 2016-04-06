d8c80808fe267b9f49334d1697ca8dec63ea3499
Celogeek <me@celogeek.com>
2016-04-06 14:47:34 +0200

    separate check

--- share/Git-Redmine-Suite/helpers/scripts/grs/git.sh
+++ share/Git-Redmine-Suite/helpers/scripts/grs/git.sh
@@ -3,7 +3,7 @@ function git_refresh_local_repos {
   git fetch --tags -p
 }
 
-function git_has_local_changes {
+function git_local_repos_is_clean {
   git_status=$(git status --untracked-files=no --porcelain)
   if [ -n "$git_status" ]
   then
@@ -15,16 +15,20 @@ function git_has_local_changes {
     return 1
   fi
 
+  return 0  
+}
+
+function git_local_repos_is_sync {
   git_remote_tracking=$(git config branch.$(git name-rev --name-only HEAD).remote)
   if [ -n "$git_remote_tracking" ]
     then
     git_cherry=$(git cherry -v --abbrev)
     if [ -n "$git_cherry" ]
     then
-      echo "There is pending commit:"
+      echo "There are pending commits:"
       echo "$git_cherry"
       echo ""
-      echo "Please push this first ..."
+      echo "Please push them first ..."
       echo ""
       return 1
     fi
